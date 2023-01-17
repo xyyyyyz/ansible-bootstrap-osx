5f771599889a39a68203d1d7727975c3e89f484a
James Koh <ext.james.koh@grabtaxi.com>
2023-01-17 16:36:11 +0800

    feat(ansible): only install ansible if not exist

--- install.sh
+++ install.sh
@@ -13,11 +13,16 @@ else
 fi
 
 # upgrade pip
-python3 -m pip install --upgrade pip
+if ! pip3 list | grep -i ansible >/dev/null; then
+  echo "Installing ansible ..."
+  python3 -m pip install --upgrade pip
 
-pip3 install ansible
+  pip3 install ansible
+else
+  echo "has ansible, skip ansible install"
+fi
 
-touch ~/.profile
+# set to path as backup
 export PATH=$PATH:~/Library/Python/3.8/bin
 
 ansible-galaxy install -r requirements.yml
